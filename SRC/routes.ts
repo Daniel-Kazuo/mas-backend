b4ef7a00799337edc49bdc72f9701fcd534d0b34
daniel-kazuo <daniel.kazuo@gmail.com>
2021-10-10 10:12:16 -0300

    Semana3
    Semana3

--- SRC/routes.ts
+++ SRC/routes.ts
@@ -1,9 +1,18 @@
-import { request, response, Router } from 'express';
+import {Router, Request, Response, response} from 'express';
+import {UserController} from './controller/UserController';
+
+interface UserRequest {
+    name:string;
+    email:string;
+    password:string;
+}
+
+const userController = new UserController()
 
 const routes = Router();
 
-routes.get('/user', (request, response) => response.json({
-    message:'hello world'
-}))
+routes.post('/user', userController.create);
+routes.post('/activy', () => console.log('Activy route'));
+routes.post('/courseunit', () => console.log('Course Unit route'));
 
 export default routes;
