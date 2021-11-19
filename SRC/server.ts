487ad1b331a45e7fe041312d9784d359ce4665cd
daniel-kazuo <daniel.kazuo@gmail.com>
2021-11-19 16:50:45 -0300

    Ajuste BackEND 19-11-2021

--- SRC/server.ts
+++ SRC/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import './database';
 import cors from 'cors';
 import routes from './routes';
+//import error from './middlewares/error';
 import config from './config/server';
 
 const app = express();
@@ -11,6 +12,6 @@ const port = config.server.port;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(Error);
 
-
-app.listen(port,()=>console.log('Server started'));
+app.listen(port, () => console.log('Server started'));
--- SRC/services/AuthenticateUserService.ts
+++ SRC/services/AuthenticateUserService.ts
@@ -1,44 +1,43 @@
-import { compare } from "bcryptjs";
-import { getRepository } from "typeorm";
-import { User } from "../model/User";
-import authConfig from "../config/auth";
-import { sign } from "jsonwebtoken";
+import {getRepository} from 'typeorm';
+import {sign} from 'jsonwebtoken';
+import {compare} from 'bcryptjs';
+import authConfig from '../config/auth';
+import {User} from '../model/User';
 
 interface AuthData {
     email: string;
-    password: string
+    password: string;
 }
 
-class AuthenticateUserService {  
+class AuthenticateUserService{
 
-    public async execute(data:AuthData):Promise<string | {}>{
-        const {email, password} = data;
+    public async execute({email,password}: AuthData): Promise<string | {}> {
 
-        const userRepository = getRepository(User);
+        const usersRepository = getRepository(User);
 
-        const user = await userRepository.findOne({email});
+        const user = await usersRepository.findOne({email});
 
-        if(!user){
+        if(!user) {
             return {
-                error:'User not found'
+                error: 'user not exist'
             }
         }
 
-        const comparePassword = await compare(password,user.password);
+        const comparePassword = await compare(password, user.password);
 
-        if(!comparePassword){
+        if(!comparePassword) {
             return {
-                error: 'Incorrect password'
+                error: 'incorrect password'
             }
         }
 
-        const {secret,expiresIn} = authConfig.jwt;
+        const { secret, expiresIn } = authConfig.jwt;
 
-        const token = sign({"role":"user"},secret,{
-            //algorithm:"RS256",
-            subject:user.id,
+        const token = sign({"role":"user"}, secret, {
+            //algorithm: 'RS256',
+            subject: user.id,
             expiresIn
-        });
+        })
 
         const {id, name, email:emailUser} = user
 
@@ -54,4 +53,4 @@ class AuthenticateUserService {
 
 }
 
-export {AuthenticateUserService}
+export {AuthenticateUserService};
